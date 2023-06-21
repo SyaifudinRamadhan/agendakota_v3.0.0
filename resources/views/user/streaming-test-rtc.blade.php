a84394fe129bcfb9d3046a07e14c6b4c5e1e30ad
SyaifudinRamadhan <syaifudinramadhan@gmail.com>
2023-06-21 20:32:19 +0700

    add test

--- resources/views/user/streaming-test-rtc.blade.php
+++ resources/views/user/streaming-test-rtc.blade.php
@@ -6,6 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Testing WebRTC</title>
+    <link rel="stylesheet" href="{{ asset('js/select2.min.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- Fevicon -->
+    <link rel="shortcut icon" href="{{ asset('images/icon-ak.png') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;600;700&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/bc29c7987f.js" crossorigin="anonymous"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    @yield('head.dependencies')
+
+    <link rel="stylesheet" href="{{ asset('css/user/newStreamPage.css') }}">
     <link rel="stylesheet" href="{{ asset('css/user/streamPage.css') }}">
 </head>
 
